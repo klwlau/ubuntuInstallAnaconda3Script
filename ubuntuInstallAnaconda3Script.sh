f844ca57e1732f661e55559917847dbf8a190ea0
Wilson.L <klwilsonlau@gmail.com>
2019-05-02 10:44:10 +0800

    remove comment

--- ubuntuInstallAnaconda3Script.sh
+++ ubuntuInstallAnaconda3Script.sh
@@ -10,9 +10,3 @@ wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
 bash Anaconda3-2019.03-Linux-x86_64.sh -b -p /home/ubuntu/anaconda
 rm Anaconda3-2019.03-Linux-x86_64.sh
 echo 'export PATH="/home/ubuntu/anaconda/bin:$PATH"' >> /home/ubuntu/.bashrc 
-
-# Reload default profile
-# sleep 5
-# source ~/.bashrc
-# sleep 5
-# conda update conda
