bea6ef4a3338652bc2734ea3384da9848873dde4
Wilson <klwilsonlau@gmail.com>
2019-09-05 20:12:01 +0800

    fix bug

--- ubuntuInstallAnaconda3Script.sh
+++ ubuntuInstallAnaconda3Script.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
 #scrpit origin https://askubuntu.com/questions/505919/how-to-install-anaconda-on-ubuntu
 # Go to home directory
 cd ~
-
 # You can change what anaconda version you want at 
 # https://repo.continuum.io/archive/
 wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
@@ -15,4 +13,3 @@ source ~/.bashrc
 
 /home/ubuntu/anaconda/bin/conda install joblib -y
 /home/ubuntu/anaconda/bin/conda update --all -y
-/home/ubuntu/anaconda/bin/jupyter notebook --generate-config
